fae6dfbca383ca1b2b949a053504234123b8c1b3
VTDZ6607 <manhhuan.nguyen@orange.com>
2023-03-28 10:01:10 +0200

    project structure

--- ticket-ui/src/app/app.module.ts
+++ ticket-ui/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { PersonComponent } from './components/person/person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
---
+++ ticket-ui/src/app/model/support-member.spec.ts
@@ -0,0 +1,7 @@
+import { SupportMember } from './support-member';
+
+describe('SupportMember', () => {
+  it('should create an instance', () => {
+    expect(new SupportMember()).toBeTruthy();
+  });
+});
