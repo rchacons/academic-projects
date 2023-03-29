5ffe44021c443a1c841db206236f9796deb6d8a9
VTDZ6607 <manhhuan.nguyen@orange.com>
2023-03-30 00:53:04 +0200

    ticket service

--- ticket-ui/src/app/service/ticket.service.ts
+++ ticket-ui/src/app/service/ticket.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Ticket } from '../model/ticket';
 
@@ -9,16 +9,25 @@ import { Ticket } from '../model/ticket';
 })
 export class TicketService {
 
-  protected getTicketUrl = `${environment.ticketApi}/ticket/`
+  protected getTicketUrl = `${environment.ticketApi}/ticket/`;
+  protected postTicketUrl = `${environment.ticketApi}/ticket/`;
 
 
   constructor(private http : HttpClient) { }
 
   getTicketById(id : string) : Observable <Ticket>  {
-    return this.http.get<Ticket>(this.getTicketById+id).pipe( 
+    return this.http.get<Ticket>(this.getTicketUrl+id).pipe( 
       tap((data)=> console.log('All:' + JSON.stringify(data)))
     );
   }
+
+  getAllTickets() : Observable <Ticket []> {
+    return this.http.get<any>(this.getTicketUrl).pipe(
+      map((data)=> data)
+    );
+  }
+
+
 
 
 }
